7bed4c98292619bb658db5feca97ad4902e476e8
NguyenHai-LETI <nguyenhaipc98@gmail.com>
2025-08-21 17:36:24 +0700

    update readme.md

--- App/auth/router.py
+++ App/auth/router.py
@@ -5,7 +5,7 @@ from App.database import get_db
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
-@router.post("/register", response_model=schemas.UserRead)
+@router.post("/register/", response_model=schemas.UserRead)
 def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = db.query(service.models.User).filter(service.models.User.email == user.email).first()
     if db_user:
--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 - uvicorn app.main:app --reload
 - fastapi dev .\main.py
 - python -m uvicorn App.main:app --reload
+
+
+##Các việc cần làm:
+- Cấu trúc dự án chuẩn có thể tham khảo: https://github.com/zhanymkanov/fastapi-best-practices
+- đọc lại về chuẩn pep8
+- đọc về quy chuẩn thiết kế API: https://learn.microsoft.com/en-us/azure/architecture/best-practices/api-design
